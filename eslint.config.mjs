4a2b8417da7fad82e4539febc71558227de850e8
Bruno Antonio Monzón <brunomonzon2002@gmail.com>
2026-05-11 16:20:03 -0400

    fix: corrige configuracion de eslint para entorno node y jest

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,17 @@
-import js from "@eslint/js";
 import globals from "globals";
-import { defineConfig } from "eslint/config";
+import pluginJs from "@eslint/js";
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-]);
+export default [
+  pluginJs.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      }
+    },
+    rules: {
+      "no-unused-vars": "error"
+    }
+  }
+];
